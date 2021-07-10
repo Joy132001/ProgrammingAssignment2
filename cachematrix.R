c7ded2f77e19291b9f95330830dbc8a6a2579dd1
Joy132001 <87224876+Joy132001@users.noreply.github.com>
2021-07-10 18:38:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a couple of operations that provides an overview of the inverse 
+## this constructs a sequence that stores the inverse
 
-## Write a short comment describing this function
+## Give a brief overview about this procedure
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        ## The inverse ascribed is filled as an outcome of this inverse
+        inverse <- NULL
+
+        ## this is in constructing the set of numbers or the matrix
+        set <-function(y) {
+                mtx <<- y;
+                inverse <<- NULL
+}
+        ## this is for acquiring the collections of numbers 
+        Get <-- function() return(mtx);
+        
+        ## to create the inverse of the set of numbers 
+        setinv <-- function (inv) inverse <<- inv;
+        
+        ## to acquire the inverse of the set of numbers
+        getinv <-- function () return(inverse); 
+        
+        ## matrix return
+        return(list(set = set, get = get, setinv = setinv, getinv = getinv)) 
 }
 
+## this is to calculate the inverse with the above 'x'.
+## this is to get the inverse of the code given above
 
-## Write a short comment describing this function
+## Retrive the inverse of 'x' as a set of number or matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## if the variables has been set, this can provide the inverse back
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+}
+        
+        ## the variable's matrix can be retrived in this method
+        data <- x$get()
+        
+        ## the inverse can be computed as an outcome of this method
+        i <- solve(data, ...)
+        
+        ## the variable's inverse can be produced as a result of this method
+        x$setinverse(i)
+        
+        ## this will provide the matrix
+        i
+        
 }
